d7b3bed444667efab51aaad1648bb92125aeb91c
Muhammad Shahzad Ali <44517352+Shahzad6077@users.noreply.github.com>
2022-06-20 22:10:32 +0500

    Updated chiper component for Compass

--- src/components/Cipher.js
+++ src/components/Cipher.js
@@ -1,12 +1,52 @@
+import { useState, useEffect } from "react"; 
 
-const Cipher = () => {
+export default function CipherCompass() {
+  const [compassCircleTransformStyle, setCompassCircleTransform] = useState(
+    "translate(-50%, -50%)"
+  );
 
-	return (
-<div>
+  const isIOS = () => {
+    return (
+      navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
+      navigator.userAgent.match(/AppleWebKit/)
+    );
+  };
 
-</div>    
-		)
+  const startCompass = () => {
+    const checkIos = isIOS();
+    console.log(checkIos);
+    if (checkIos) {
+      DeviceOrientationEvent.requestPermission()
+        .then((response) => {
+          if (response === "granted") {
+            window.addEventListener("deviceorientation", handler, true);
+          } else {
+            alert("has to be allowed!");
+          }
+        })
+        .catch(() => alert("not supported"));
+    } else {
+      window.addEventListener("deviceorientationabsolute", handler, true);
+    }
+  };
+  const handler = (e) => {
+    const compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
+    const compassCircleTransform = `translate(-50%, -50%) rotate(${-compass}deg)`;
+    setCompassCircleTransform(compassCircleTransform);
+  };
+  return (
+    <div className="compassWrapper"> 
+      <div className="compass">
+        <div className="arrow" />
+        <div
+          className="compass-circle"
+          style={{ transform: compassCircleTransformStyle }}
+        />
+        <div className="my-point" />
+      </div>
+      <button className="start-btn" onClick={startCompass}>
+        Start compass
+      </button>
+    </div>
+  );
 }
-
-export default Cipher
-
